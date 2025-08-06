b2ef429566f6a4de1012aede54516d09acf8ea19
Dushali-2003 <dushali@aerinit.com>
2025-08-06 14:53:11 +0530

    End of lab:launching VMs

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ key = "student.50-network-state"
 region = "us-east-2"
 }
 }
+resource "aws_instance" "my_vm" {
+ami = "ami-003932de22c285676"
+subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+instance_type = "t3.micro"
+tags = {
+Name = "student.50-vm1"
+}
+}
