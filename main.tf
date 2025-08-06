ee2a22e55ddda9d4122a4077507ef9a7e05c7574
Dushali-2003 <dushali@aerinit.com>
2025-08-06 15:50:48 +0530

    ssh successful

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ ami = "ami-003932de22c285676"
 subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
 instance_type = "t3.micro"
 key_name = data.terraform_remote_state.network_details.outputs.my_key
+vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
 tags = {
 Name = "student.50-vm1"
 }
