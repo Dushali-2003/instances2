ff64e013a2e024febe6ce1b80f3eed03141b15d4
Dushali-2003 <dushali@aerinit.com>
2025-08-06 17:38:30 +0530

    Child node

--- main.tf
+++ main.tf
@@ -6,11 +6,12 @@ key = "student.50-network-state"
 region = var.region
 }
 }
-resource "aws_instance" "my_vm" {
+module "webserver" {
+source = "./modules/linux_node"
 ami = "ami-003932de22c285676"
-subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
 instance_type = "t3.micro"
 key_name = data.terraform_remote_state.network_details.outputs.my_key
+subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
 vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
 tags = {
 Name = "student.50-vm1"
