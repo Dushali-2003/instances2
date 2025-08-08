356a57bf1277dc0c1a0b85eb77c5e67183421aad
Dushali-2003 <dushali@aerinit.com>
2025-08-08 13:10:44 +0530

    Complete

--- modules/linux_node/instances.tf
+++ modules/linux_node/instances.tf
@@ -1,36 +1,39 @@
 resource "aws_instance" "my_vm" {
-count = var.instance_count
-ami = var.ami
-subnet_id = var.subnet_id
-instance_type = var.instance_type
-key_name = var.key_name
-vpc_security_group_ids = var.vpc_security_group_ids
-tags = var.tags 
+  count                  = var.instance_count
+  ami                    = var.ami
+  subnet_id              = var.subnet_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = var.vpc_security_group_ids
+  tags                   = var.tags
 }
 
 resource "local_file" "tf_ansible_inventory" {
-count = length(aws_instance.my_vm)>0 ? 1 : 0
-content = <<-EOT
-[${var.install_package}]
-%{ for vm in aws_instance.my_vm.* ~}
+  count    = length(aws_instance.my_vm) > 0 ? 1 : 0
 
-${vm.private_dns} ansible_host=${vm.public_ip}
-ansible_ssh_user=ubuntu
-%{ endfor ~}
+  content  = <<-EOT
+[${var.install_package}]
+%{for vm in aws_instance.my_vm[*]}
+${vm.private_dns} ansible_host=${vm.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/dushali/terraform_base2/keys2/student.50-vm-key ansible_ssh_common_args='-o IdentitiesOnly=yes'
+%{endfor}
 EOT
-filename = "./tf_ansible_${var.install_package}_inventory.ini"
+
+  filename = "./tf_ansible_${var.install_package}_inventory.ini"
 }
 
 resource "time_sleep" "wait_30_seconds" {
-depends_on = [aws_instance.my_vm]
-count = length(aws_instance.my_vm)>0 ? 1 : 0
-create_duration = "30s"
+  depends_on      = [aws_instance.my_vm]
+  count           = length(aws_instance.my_vm) > 0 ? 1 : 0
+  create_duration = "30s"
 }
 
 resource "null_resource" "install_package" {
-count = length(aws_instance.my_vm)>0 ? 1 : 0
-depends_on = [time_sleep.wait_30_seconds]
-provisioner "local-exec" {
-command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -i /home/dushali/terraform_base2/ansible_inventory.yaml -i /home/dushali/terraform_base2/ansible_webservers_inventory.yaml /home/dushali/terraform_base2/ansible-playbooks/${var.playbook_name} --private-key '/home/dushali/terraform_base2/keys2/student.50-vm-key' "
-}
+  count      = length(aws_instance.my_vm) > 0 ? 1 : 0
+  depends_on = [time_sleep.wait_30_seconds]
+  provisioner "local-exec" {
+    environment = {
+      ANSIBLE_SSH_COMMON_ARGS = " -o IdentitiesOnly=yes"
+    }
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i ./tf_ansible_${var.install_package}_inventory.ini -i ./tf_ansible_webservers_inventory.ini /home/dushali/terraform_base2/ansible-playbooks/${var.playbook_name} --private-key '../../../keys2/student.50-vm-key'"
+  }
 }
