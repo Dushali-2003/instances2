de31ee716bfc249da6366060e5f35b21c223966f
Dushali-2003 <dushali@aerinit.com>
2025-08-11 14:24:28 +0530

    lab end

--- output.tf
+++ output.tf
@@ -4,3 +4,9 @@ value = module.webserver.*.public_ip
 output "loadbalancer_vm_public_ip" {
 value = module.loadbalancer.*.public_ip
 }
+output "web_docker_host_vm_public_ip" {
+value = module.web_docker_host.*.public_ip
+}
+output "lb_docker_host_public_ip" {
+value = module.lb_docker_host.*.public_ip
+}
